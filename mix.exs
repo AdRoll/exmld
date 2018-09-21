c4d57ccde40935eb57f5bbf27dfd8c3173e0a68c
Mike Watters <mike.watters@adroll.com>
2018-09-21 11:34:46 -0600

    update erlmld dep

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Exmld.Mixfile do
   use Mix.Project
 
-  @version "0.1.6"
+  @version "0.1.7"
   @name "exmld"
   @repo "https://github.com/AdRoll/#{@name}"
 
@@ -31,7 +31,7 @@ defmodule Exmld.Mixfile do
   defp deps do
     [
       {:flow, "~> 0.14.2"},
-      {:erlmld, "~> 0.1.9"},
+      {:erlmld, "~> 0.1.10"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
